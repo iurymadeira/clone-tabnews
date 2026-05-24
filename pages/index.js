4ee7e2b4476de58a15b78c49b15a96287e7d5641
Iury Madeira <iurymadeiraaa@gmail.com>
2026-05-24 07:59:55 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>eu sou brabo e voces uns cocô 💩</h1>
+  return <h1>eu sou brabo e voces uns cocô 💩</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentaçâo errada");
 }
 
 export default Home;
