ec2da1398cb368e603b436a4d9ceea67818570a4
Iury Madeira <iurymadeiraaa@gmail.com>
2026-05-28 11:17:59 +0000

    add os arquivos database.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, reponse) {
+import database from "../../../../infra/database.js";
+
+async function status(request, reponse) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   reponse.status(200).json({ chave: "testando response" });
 }
 
