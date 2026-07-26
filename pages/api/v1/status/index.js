22a1cde99c087eefbf94214e68e7dff599d367f7
Iury Madeira <iurymadeiraaa@gmail.com>
2026-07-26 04:16:51 +0000

    add arquivo jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, reponse) {
   const result = await database.query("SELECT 1 + 1 as sum;");
