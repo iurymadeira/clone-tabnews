33dbf5ff569e1fac35f53323d8d5eee0f2c14444
Iury Madeira <iurymadeiraaa@gmail.com>
2026-08-01 23:01:38 +0000

    finaliza o endpoint /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
-async function status(request, reponse) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  reponse.status(200).json({ chave: "testando response" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
+
+  const dataBaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname =$1;",
+    values: [dataBaseName],
+  });
+  const dataBaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: dataBaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
